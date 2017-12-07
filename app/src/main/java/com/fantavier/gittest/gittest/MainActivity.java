c0b3c4e32f45f6d7a2067c832e2f028fcea2ae64
JuriBog <juri.bogatyrjow@stud.hs-hannover.de>
2017-12-07 12:38:57 +0100

    App ist fertig

--- app/src/main/java/com/fantavier/gittest/gittest/MainActivity.java
+++ app/src/main/java/com/fantavier/gittest/gittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /* Paul hat bearbeitet */
         /*Juri hat bearbeitet*/
+        /*Ich wieder*/
     }
 }
