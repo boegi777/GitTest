8add9fb878ce3e5342531a6be54c6d90a2d26823
Paul Bögelsack <paul.boegelsack@gmx.de>
2017-12-07 12:29:45 +0100

    Paul hat schon wieder bearbeitet

--- app/src/main/java/com/fantavier/gittest/gittest/MainActivity.java
+++ app/src/main/java/com/fantavier/gittest/gittest/MainActivity.java
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /* Paul hat bearbeitet */
     }
+
+    /* Paul hat hier auch bearbeitet */
 }
